b8f17ef5afaaf14984ee3a99d4c5f65fd1fea3ef
Robert J Samson <rjsamson@me.com>
2020-07-15 01:00:05 -0400

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,53 @@
-import Head from 'next/head'
+import Head from "next/head";
+import _ from "lodash";
+import {
+  ComposedChart,
+  Line,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Label,
+} from "recharts";
+// import data from "../data/covid-data.json";
 
-export default function Home() {
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch(
+    "https://health.data.ny.gov/resource/xdss-u53e.json?county=Albany"
+  );
+  const data = await res.json();
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Home({ data }) {
+  const albanyData = _.chain(data)
+    .map((results) => {
+      return {
+        ...results,
+        positive_rate: results.new_positives / results.total_number_of_tests,
+        positive_rate_percentage: (
+          (results.new_positives / results.total_number_of_tests) *
+          100
+        ).toFixed(2),
+      };
+    })
+    .filter({ county: "Albany" })
+    .sortBy(["test_date"])
+    // .takeRight(30)
+    .value();
+  console.log(albanyData);
+
   return (
     <div className="container">
       <Head>
@@ -9,54 +56,101 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="chart-row">
+          <div>
+            <span className="chart-title">Albany County</span>
+            <ComposedChart
+              width={500}
+              height={300}
+              data={albanyData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="test_date" label={false} tick={false} />
+              <YAxis yAxisId="left" />
+              <YAxis yAxisId="right" orientation="right" domain={[0, 25]} />
+              <Tooltip />
+              <Legend />
+              <Bar
+                stackId="a"
+                dataKey="new_positives"
+                fill="#8884d8"
+                label={false}
+                yAxisId="left"
+              />
+              <Bar
+                stackId="a"
+                dataKey="total_number_of_tests"
+                fill="#82ca9d"
+                label={false}
+                yAxisId="left"
+              />
+              <Line
+                type="monotone"
+                dataKey="positive_rate_percentage"
+                stroke="#ff7300"
+                yAxisId="right"
+                dot={false}
+              />
+            </ComposedChart>
+          </div>
+          <div>
+            <span className="chart-title">Albany County</span>
+            <ComposedChart
+              width={500}
+              height={300}
+              data={albanyData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="test_date" label={false} tick={false} />
+              <YAxis yAxisId="left" />
+              <YAxis yAxisId="right" orientation="right" domain={[0, 25]} />
+              <Tooltip />
+              <Legend />
+              <Bar
+                stackId="a"
+                dataKey="new_positives"
+                fill="#8884d8"
+                label={false}
+                yAxisId="left"
+              />
+              <Bar
+                stackId="a"
+                dataKey="total_number_of_tests"
+                fill="#82ca9d"
+                label={false}
+                yAxisId="left"
+              />
+              <Line
+                type="monotone"
+                dataKey="positive_rate_percentage"
+                stroke="#ff7300"
+                yAxisId="right"
+                dot={false}
+              />
+            </ComposedChart>
+          </div>
         </div>
       </main>
 
       <footer>
+        <span>© Capital Region Covid Project</span>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://forward.ny.gov/percentage-positive-results-region-dashboard"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        ></a>
       </footer>
 
       <style jsx>{`
@@ -67,6 +161,20 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+        }
+
+        .chart-row {
+          flex: 1;
+          display: flex;
+          flex-direction: row;
+        }
+
+        .chart-title {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          text-align: center;
+          margin-bottom: 1rem;
         }
 
         main {
@@ -102,42 +210,6 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
         .grid {
           display: flex;
           align-items: center;
@@ -146,36 +218,6 @@ export default function Home() {
 
           max-width: 800px;
           margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
         }
 
         .logo {
@@ -189,21 +231,6 @@ export default function Home() {
           }
         }
       `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
-  )
+  );
 }

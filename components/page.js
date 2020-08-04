747cbc74f9bd3cae6ba1bde7829b0ba02a712502
Robert J Samson <rjsamson@me.com>
2020-08-04 16:54:39 -0400

    More styling

--- components/page.js
+++ components/page.js
@@ -2,10 +2,12 @@ import Header from "./header";
 
 export default function Page({ children }) {
   return (
-    <div className="flex-col justify-center">
-      <Header />
-      {children}
-      <footer>
+    <div className="w-screen">
+      <div className="flex-col justify-center">
+        <Header />
+        {children}
+      </div>
+      <footer className="flex justify-center items-center border-t border-gray-500 w-screen">
         <span>© Capital Region Covid Project</span>
       </footer>
     </div>
